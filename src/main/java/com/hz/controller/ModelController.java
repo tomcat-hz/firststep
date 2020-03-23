eaf7ba001bbf6a0f60afe324b4b895a3210e9010
tomcat-hz <2218359849@qq.com>
2020-03-23 19:25:08 +0800

    spring boot项目初始化

---
+++ src/main/java/com/hz/controller/ModelController.java
@@ -0,0 +1,14 @@
+package com.hz.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @version V1.0
+ * @author: hezheng
+ * @date: 2020/1/6 11:10
+ */
+@Controller
+public class ModelController {
+
+
+}
